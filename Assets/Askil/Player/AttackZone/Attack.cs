cd2b733934ce76d2ae33fd59c1febc5bec71c63d
askilform <askilform@gmail.com>
2025-08-18 15:19:03 +0200

    enkel lydeffekter

--- Assets/Askil/Player/AttackZone/Attack.cs
+++ Assets/Askil/Player/AttackZone/Attack.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Attack : MonoBehaviour
@@ -6,40 +7,52 @@ public class Attack : MonoBehaviour
     private int ChargeLevel = 0;
     private Collider ringCollider;
 
+    public TextMeshProUGUI ChargeTimeTXT;
+    public TextMeshProUGUI ChargeLevelTXT;
+    public AudioSource riseSFX;
+    public AudioSource smackSFX;
+
     private void Start()
     {
         ringCollider = GetComponent<Collider>();
+        ChargeTimeTXT.text = "Charged For: " + Mathf.Round(ChargedFor * 100f) / 100f;
+        ChargeLevelTXT.text = "ChargeLevel: " + ChargeLevel;
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ChargedFor = 0f; // reset charge when starting
+            ChargedFor = 0f;
             ChargeLevel = 1;
-            print("Charge Level: " + ChargeLevel);
+            riseSFX.Play();
         }
 
         if (Input.GetMouseButton(0))
         {
             ChargedFor += Time.deltaTime;
+            ChargeTimeTXT.text = "Charged For: " + Mathf.Round(ChargedFor * 100f) / 100f;
+            ChargeLevelTXT.text = "ChargeLevel: " + ChargeLevel;
 
             if (ChargedFor >= 2f && ChargeLevel < 2)
             {
                 ChargeLevel = 2;
-                print("Charge Level: " + ChargeLevel);
             }
 
             if (ChargedFor >= 4f && ChargeLevel < 3)
             {
                 ChargeLevel = 3;
-                print("Charge Level: " + ChargeLevel);
             }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            print("Attack! Charge Level: " + ChargeLevel);
+            ChargedFor = 0f;
+            ChargeLevel = 1;
+            ChargeTimeTXT.text = "Charged For: " + Mathf.Round(ChargedFor * 100f) / 100f;
+            ChargeLevelTXT.text = "ChargeLevel: " + ChargeLevel;
+            riseSFX.Stop();
+            smackSFX.Play();
         }
     }
 }
